cdbffcf1e1fd6289222586f6962ee7e0ec11de44
fwardzic <fwardzic@cisco.com>
2021-03-23 18:52:42 +0100

    added nginx

---
+++ nginx/index.php
@@ -0,0 +1,3 @@
+<?php echo " \r\n THIS IS ".php_uname('n')." WEB SERVER"."\r\n"; ?>
+<?php echo " \r\n This server IP: ".$_SERVER['HTTP_HOST']."\r\n"; ?>
+<?php echo " \r\n Your IP address: ".$_SERVER['REMOTE_ADDR']."\r\n"; ?> 
